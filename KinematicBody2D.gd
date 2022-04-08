09ae98d5b50e67cf7c9a1e15eac370967b297edd
Golobi <101245812+Golobi@users.noreply.github.com>
2022-04-08 12:29:26 +0200

    Personatge 1r dia

---
+++ KinematicBody2D.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (float) var rotation_speed = 3
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed("Dreta"):
+		rotation_dir += 1
+	if Input.is_action_pressed("Esquerra"):
+		rotation_dir -= 1
+	if Input.is_action_pressed("Baixa"):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("Puja"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_just_pressed("Dispara"):
+		
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
+
+
