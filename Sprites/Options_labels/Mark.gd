b28c31c84232014b526bc86c8d3c410a7a2cf387
Salipoke <101245660+Salipoke@users.noreply.github.com>
2022-04-24 17:34:40 +0200

    Tot el menu 100% funcional
    FPS ja funciona, transicions funcionen

--- Sprites/Options_labels/Mark.gd
+++ Sprites/Options_labels/Mark.gd
@@ -5,9 +5,9 @@ extends Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position[0] = 780
 
 func _input(event:InputEvent):
 	if event is InputEventMouseButton and event.pressed:
-		var bounds = Rect2(Vector2(714,77),Vector2(178.206))
-		if bounds.has_point(event.position[0]-rect_position[0]p)
+		if event.position[0] > 720 and event.position[0] < (721+170):
+			position[0] = event.position[0]
