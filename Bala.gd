e3c2250dcb6115c05bc4f401f5cd98f98af4df80
Golobi <101245812+Golobi@users.noreply.github.com>
2022-04-26 23:54:24 +0200

    Verdader Bala part 2

--- Bala.gd
+++ Bala.gd
@@ -8,3 +8,7 @@ func fire(origin : Vector2, dest : Vector2):
 func _process(delta):
 	if dir:
 		global_position += dir * delta * 350
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
