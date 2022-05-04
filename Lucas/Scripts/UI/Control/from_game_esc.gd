2c26156fe14f8e125164085639ed458f367563ae
Rowlet20 <101245778+Rowlet20@users.noreply.github.com>
2022-05-04 08:19:38 +0200

    Arreglat temporalment
    Comença a funcionar després de la tocada de collons del Arnau, té els dies contats.

--- Lucas/Scripts/UI/Control/from_game_esc.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-#DESDE EL JOC, SI PREMS ESC, VA AL MENU
-
-func _input(InputEvent):
-	if Input.is_action_just_pressed("ui_home"):
-		get_tree().change_scene("res://Lucas/Scenes/UI/labels/menu.tscn")
--- Lucas/Scripts/UI/Control/options_back.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-#DESDE LA PESTANYA D'OPCIONS, SI PREMS ESC, VA AL MENU
-
-func _input(event:InputEvent):
-	if event is InputEventKey and event.is_action_pressed("ui_home"):
-			get_tree().change_scene("res://Lucas/Scenes/UI/labels/menu.tscn")
