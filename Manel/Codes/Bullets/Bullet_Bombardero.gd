2169cf41a15bd2c974d0b1430c7a57e4f9ff3978
Rowlet20 <101245778+Rowlet20@users.noreply.github.com>
2022-05-16 12:29:02 +0200

    Enemic Bombardero - Part 1
    Falta q la bala es quedi quieta en el lloc i acabar la part en la q el target se'n va.

---
+++ Manel/Codes/Bullets/Bullet_Bombardero.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var move = Vector2.ZERO
+var look_player = Vector2.ZERO
+var player = null
+var vel = 3
+
+func _ready():
+	look_player = player.position - global_position
+
+func _physics_process(delta):
+	move = Vector2.ZERO
+	
+	move = move.move_toward(look_player, delta)
+	move = move.normalized() * vel
+	position += move
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
---
+++ Manel/Codes/Enemies/Enemic_Bombardero.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+onready var BULLET_SCENE = preload("res://Manel/Scenes/Bullets/Bullet_base.tscn")
+onready var TARGET = preload("res://Manel/Scenes/Bullets/Target.tscn")
+
+var health = 500
+var damage = 50
+var player = null
+
+func _physics_process(delta):
+	#Health
+	if 1 == 1: #posar un if q funcioni quan la area de la bala del jugador doni al enemic
+		health -= 100
+	if health <= 0:
+		pass
+
+func _on_Area2D_body_entered(body):
+	if body.name == "jugateur": #quan s'agunti tot es té que canviar al nom del jugador
+		player = body
+
+func fire():#https://godotengine.org/qa/81726/how-to-make-enemy-shoot-at-player
+	var bullet = BULLET_SCENE.instance()
+	var target = TARGET.instance()
+	#Target
+	target.position = player.position
+	get_parent().add_child(target)
+	$Target.set_wait_time(2)
+	#Bullet
+	bullet.position = get_global_position()
+	bullet.player = player
+	get_parent().add_child(bullet)
+	$Timer.set_wait_time(5)
+
+func _on_Timer_timeout():
+	if player != null:
+		fire()
+
+func _on_Target_timeout():
+	target.position.y = 999
