03130b73c3f05a7f4846f1e33c4b332c9bc73ab7
Salipoke <101245660+Salipoke@users.noreply.github.com>
2022-05-23 12:32:13 +0200

    enemics afegits
    Falta comprovar si els enemics estan morts

--- Lucas/Scripts/UI/Control/inside_level_control.gd
+++ Lucas/Scripts/UI/Control/inside_level_control.gd
@@ -4,5 +4,6 @@ func _input(event:InputEvent):
 	if event is InputEventKey and event.is_action_pressed("ui_home"):
 		get_tree().change_scene('res://Lucas/Scenes/UI/Control/between_lvl.tscn')
 
-func _ready():
-	pass
+func _process(delta):
+	if get_tree().get_nodes_in_group("enemie").size() == 0:
+		print('a')
