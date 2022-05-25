168c99f9b29392cab8f65cd606d16301ba6528b2
Salipoke <101245660+Salipoke@users.noreply.github.com>
2022-05-26 01:51:45 +0200

    Joc acabat

--- Manel/Codes/Enemies/Enemic_3bales.gd
+++ Manel/Codes/Enemies/Enemic_3bales.gd
@@ -28,7 +28,6 @@ func _physics_process(delta):
 			pass
 		else:
 			move = position.direction_to(player.position) * vel
-		
 		$Sprite.look_at(player.global_position)
 		
 	move = move_and_collide(move)
